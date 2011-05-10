801bb0e408fa5e5d29ae832753dbac8dee24ab28
eatonmi <eatonmi@rose-hulman.edu>
2011-05-10 19:21:40 -0400

    Committed changes to get all pre-written tests and features done.

--- features/step_definitions/fortune_steps.rb
+++ features/step_definitions/fortune_steps.rb
@@ -1,5 +1,5 @@
 Then /^I should see a fortune$/ do
  fortune = page.find (".fortune")
- fortune.text.should match /~\n.*\n\s*$/
+ fortune.text.should match /^\n.*\n\s*$/
 end
 
--- fortune.rb
+++ fortune.rb
@@ -4,4 +4,12 @@ require 'thin'
 
 class Fortune < Sinatra::Base
 
+get '/fortune/random/?' do
+ @fortune = $fortunes[rand($fortunes.size)]
+ haml :fortune
 end
+
+
+end
+
+require 'fortunedata'
